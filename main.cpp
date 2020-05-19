bae1973cf687c5262370cdd20560cdfd5e0284e5
Malcon Rezende <rezendemalcon13@gmail.com>
2020-05-19 07:39:41 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout <<endl<< "SISTEMA DE PERFIL DE CLIENTE" <<endl;
+    cout << "------------------------------" <<endl;
+    cout << "INFORME OS DADOS DO ULTIMO ANO" <<endl<<endl;
+
     return 0;
 }
