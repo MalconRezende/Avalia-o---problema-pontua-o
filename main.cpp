527d3cf24850274f1d1dbafc7e2ee4501f030caf
Malcon Rezende <rezendemalcon13@gmail.com>
2020-05-19 08:27:08 -0300

    Score de volume de compras

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main()
     double ComprarCliente, TicketMedio;
     double PagamentoAtraso;
     string FormaDepagamento;
+    double ScoreDeCompra;
+
+    ScoreDeCompra = 0;
 
     //CABECALHO
 
@@ -16,7 +19,7 @@ int main()
 
     // LEITURA DO VOLUME
 
-    cout << "Quantas comprar o cliente fez no ultimo ano? ";
+    cout << "Quantas compras o cliente fez no ultimo ano? ";
     cin >> ComprarCliente;
     cout << "Qual o ticket medio? ";
     cin >> TicketMedio;
@@ -28,6 +31,22 @@ int main()
     cout << "A maioria das compras foi em dinheiro, cartao, ou  boleto (D/C/B)? ";
     cin >> FormaDepagamento;
 
+    //SCORE DE VOLUME DE COMPRAS
+
+    if (ScoreDeCompra = 0){
+        ScoreDeCompra = 0;
+    }
+    if (ScoreDeCompra < 3000){
+        ScoreDeCompra = 20;
+    }
+    if (ScoreDeCompra = 3000){
+        ScoreDeCompra = 40;
+    }
+    if (ScoreDeCompra > 3000){
+        ScoreDeCompra = 60;
+    }
+
+    cout <<endl<< "Score de volume de compras = " << ScoreDeCompra << " pontos";
 
     return 0;
 }
