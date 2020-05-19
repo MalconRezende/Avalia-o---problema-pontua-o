7a59bc677c8d65d427338369dde48eb00a2ddd10
Malcon Rezende <rezendemalcon13@gmail.com>
2020-05-19 07:47:56 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@ using namespace std;
 
 int main()
 {
+    double ComprarCliente, TicketMedio;
+
+    //CABECALHO
+
     cout <<endl<< "SISTEMA DE PERFIL DE CLIENTE" <<endl;
     cout << "------------------------------" <<endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" <<endl<<endl;
+
+    // LEITURA DE DADOS
+
+    cout << "Quantas comprar o cliente fez no ultimo ano? ";
+    cin >> ComprarCliente;
+    cout << "Qual o ticket medio? ";
+    cin >> TicketMedio;
 
     return 0;
 }
