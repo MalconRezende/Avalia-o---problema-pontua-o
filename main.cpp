0197b17c4ee36702356afca0bca36e660aa284df
Malcon Rezende <rezendemalcon13@gmail.com>
2020-05-19 07:59:57 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main()
 {
     double ComprarCliente, TicketMedio;
+    double PagamentoAtraso;
+    string FormaDepagamento;
 
     //CABECALHO
 
@@ -12,12 +14,20 @@ int main()
     cout << "------------------------------" <<endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" <<endl<<endl;
 
-    // LEITURA DE DADOS
+    // LEITURA DO VOLUME
 
     cout << "Quantas comprar o cliente fez no ultimo ano? ";
     cin >> ComprarCliente;
     cout << "Qual o ticket medio? ";
     cin >> TicketMedio;
+
+    // LEITURA DOS DADOS
+
+    cout <<endl<< "Quantas vezes o cliente atrasou o pagamento? ";
+    cin >> PagamentoAtraso;
+    cout << "A maioria das compras foi em dinheiro, cartao, ou  boleto (D/C/B)? ";
+    cin >> FormaDepagamento;
+
 
     return 0;
 }
